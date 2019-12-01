560f8927c0fb340d3be339bd76536b36fb6ca851
Artem Tkachuk <arti17900@gmail.com>
2019-12-02 00:33:33 +0600

    Добавлено четыре точки входа в приложение api.

--- api-project/api/urls.py
+++ api-project/api/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('add/', views.add_view, name='add'),
+    path('subtract/', views.subtract_view, name='subtract'),
+    path('multiply/', views.multiply_view, name='multiply'),
+    path('divide/', views.divide_view, name='divide'),
+]
--- api-project/api/views.py
+++ api-project/api/views.py
@@ -1,3 +1,117 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+def error_func(status, message):
+    response = JsonResponse({'error': message})
+    response.status_code = status
+    return response
+
+
+def check_func(a, b):
+    try:
+        int(a)
+        int(b)
+        return True
+    except ValueError:
+        return False
+
+
+@csrf_exempt
+def add_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        if request.body:
+            request_data = json.loads(request.body)
+            A = request_data.get('A')
+            B = request_data.get('B')
+
+            if check_func(A, B):
+                result = int(A) + int(B)
+            else:
+                return error_func(400, 'Value Error')
+
+            data = {
+                'answer': result
+            }
+            return JsonResponse(data)
+
+        else:
+            return error_func(400, 'No data provided!')
+    else:
+        return error_func(405, 'Method not allowed')
+
+
+@csrf_exempt
+def subtract_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        if request.body:
+            request_data = json.loads(request.body)
+            A = request_data.get('A')
+            B = request_data.get('B')
+
+            if check_func(A, B):
+                result = int(A) - int(B)
+            else:
+                return error_func(400, 'Value Error')
+
+            data = {
+                'answer': result
+            }
+            return JsonResponse(data)
+
+        else:
+            return error_func(400, 'No data provided!')
+    else:
+        return error_func(405, 'Method not allowed')
+
+
+@csrf_exempt
+def multiply_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        if request.body:
+            request_data = json.loads(request.body)
+            A = request_data.get('A')
+            B = request_data.get('B')
+
+            if check_func(A, B):
+                result = int(A) * int(B)
+            else:
+                return error_func(400, 'Value Error')
+
+            data = {
+                'answer': result
+            }
+            return JsonResponse(data)
+
+        else:
+            return error_func(400, 'No data provided!')
+    else:
+        return error_func(405, 'Method not allowed')
+
+
+@csrf_exempt
+def divide_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        if request.body:
+            request_data = json.loads(request.body)
+            A = request_data.get('A')
+            B = request_data.get('B')
+
+            if B == 0:
+                return error_func(400, 'Division by zero!')
+
+            if check_func(A, B):
+                result = int(A) / int(B)
+            else:
+                return error_func(400, 'Value Error')
+
+            data = {
+                'answer': result
+            }
+            return JsonResponse(data)
+
+        else:
+            return error_func(400, 'No data provided!')
+    else:
+        return error_func(405, 'Method not allowed')
