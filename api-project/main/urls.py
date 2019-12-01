ae85b5331b687e21f321a2ec92f7821230e7a72b
Artem Tkachuk <arti17900@gmail.com>
2019-12-01 21:01:00 +0600

    Добавлено приложение api.

--- api-project/main/urls.py
+++ api-project/main/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('webapp.urls')),
+    path('', include('webapp.urls')),
+    path('api/', include('api.urls')),
 ]
--- api-project/webapp/views.py
+++ api-project/webapp/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request, *args, **kwargs):
-    return HttpResponse('Worked')
+    return render(request, 'index.html')
